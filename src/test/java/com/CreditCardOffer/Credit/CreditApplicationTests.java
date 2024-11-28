d24bc82f8c7d541b440d3eae060191c0fd4beca8
VinayKThota <vinaykrishnathota017@gmail.com>
2024-11-28 16:01:49 -0500

    Initial commit

---
+++ src/test/java/com/CreditCardOffer/Credit/CreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CreditCardOffer.Credit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
